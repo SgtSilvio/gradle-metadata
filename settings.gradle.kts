ec27c99edb8d13297a3c16551d1e37ee138a894c
Silvio Giebl <silvio.giebl@hivemq.com>
2021-03-23 00:27:36 +0100

    Cleanup settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,8 @@
+rootProject.name = "gradle-metadata"
+
 pluginManagement {
     plugins {
         kotlin("jvm") version "${extra["plugin.kotlin.version"]}"
         id("com.gradle.plugin-publish") version "${extra["plugin.plugin-publish.version"]}"
     }
 }
-
-rootProject.name = "gradle-metadata"
